45e4b84ee4fb3aea19677b5b1e92c0f4941d057e
mszczepanskigit <mateusz.szczepanski.kontakt@gmail.com>
2023-04-05 18:41:52 +0200

    Lab6

--- matlab/Laby_6.m
+++ matlab/Laby_6.m
@@ -6,7 +6,7 @@ mask = 1/159 * [2  4  5  4 2;
                 4  9 12  9 4;
                 2  4  5  4 2];
 
-%p�tla dodana na lab5.1
+%pętla dodana na lab5.1
 for i=1:10
 	Boat = conv2(Boat, mask, 'same');
 end;
@@ -22,32 +22,27 @@ table_y = table_x';
 gradient_x = conv2(Boat, table_x, 'same');
 gradient_y = conv2(Boat, table_y, 'same');
 modules = sqrt(gradient_x.^2 + gradient_y.^2);
-T = angle(gradient_x + i*gradient_y);
+T = angle(complex(gradient_x, gradient_y));
 %figure(3); colormap(gray(256)); image(100*(T + pi))
-
-%figure(3); colormap(gray(256)); image(modules)
-
-
+%figure(3.1); colormap(gray(256)); image(modules)
 for i=1:512
     for j=1:512
         if abs(T(i,j))<=pi/8 || abs(T(i,j))>=7*pi/8
             T(i,j)=1;
-        else
-            if T(i,j)>=5*pi/8 || T(i,j)>=-3*pi/8
-                T(i,j)=2;
-            else
-                if T(i,j)>=3*pi/8 || T(i,j)>=-5*pi/8
-                    T(i,j)=3;
-                else
-                    T(i,j)=4;
-                end;
-            end;
-        end;
-    end;
-end;
+		  elseif T(i,j)>=5*pi/8 || (T(i,j)>=-3*pi/8 && T(i,j)<0)
+				T(i,j)=2;
+		  elseif T(i,j)>=3*pi/8 || (T(i,j)>=-5*pi/8 && T(i,j)<0)
+				T(i,j)=3;
+		  else
+				T(i,j)=4;
+		  endif;
+	endfor;
+endfor;
+	 
+
 %figure(4); colormap(gray(256)); image(50*T)
 
-%znajdujemy maksima gradientów w kierunku gradientów
+%znajdujemy maksima w kierunku gradientów
 G = modules;
 Gt = zeros(512);
 for i=2:511
@@ -83,5 +78,43 @@ for i=1:512
 	endfor;
 endfor;
 figure(7); colormap(gray(256)); image(Edges)
-	
+
+%od tego miejsca mamy lab6
+F = Edges;
+thresh_low = 5;
+for k=1:20
+	for i=2:511
+		 for j=2:511
+			  if Edges(i,j) == 255
+				if T(i,j)== 1
+					i1 = i-1;
+					i2 = i+1;
+					j1=j;
+					j2=j;
+				elseif T(i,j)==2
+					i1 = i-1;
+					i2 = i+1;
+					j1=j+1;
+					j2=j-1;
+				elseif T(i,j)==3
+					i1 = i;
+					i2 = i;
+					j1=j-1;
+					j2=j+1;
+				else
+					i1 = i-1;
+					i2 = i+1;
+					j1=j-1;
+					j2=j+1;
+			  endif
+			  if Gt(i1,j1)>thresh_low || Gt(i2,j2)>thresh_low
+				Edges(i1,j1)=127;  
+			  endif
+		  endif
+	  endfor
+	endfor
+endfor
+Edges(Edges==127)=255;
+figure(8); colormap(gray(256)); image(Edges)	  
+
 
